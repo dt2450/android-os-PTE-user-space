0b411129c6753a8cd80d2eabad14a96c1d01d862
Devashi Tandon <dtandon2450@columbia.edu>
2014-11-20 18:51:59 -0500

    Final version of test program

--- vm_inspector/vm_inspector.c
+++ vm_inspector/vm_inspector.c
@@ -39,13 +39,10 @@ static void validate_args(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	int ret = -1;
-	//int i, j;
 	unsigned long virt_addr;
 	int fd = -1;
 	void *mmap_addr = NULL;
-	//unsigned long *pte_base = NULL;
 	unsigned long page_phy_addr = NULL;
-	//unsigned long pte_entry;
 	unsigned long *fake_pgd_base = NULL;
 	char unique_file_name[MAX_FILE_SIZE] = "";
 
@@ -99,49 +96,43 @@ int main(int argc, char **argv)
 	}
 
 	/*
-	 * [index] [virt] [phys] [young bit] [file bit] [dirty bit] [read-only
-	 * bit] [xn bit]
-	 * e.g.
-	 * 0x0 0x00000000 0x530000 1 1 0 1 1
-	 * */
+	* [index] [virt] [phys] [young bit] [file bit] [dirty bit] [read-only
+	* bit] [xn bit]
+	* e.g.
+	* 0x0 0x00000000 0x530000 1 1 0 1 1
+	* */
 	printf("mmap start: %x\n", (unsigned int)mmap_addr);
 	printf("mmap end: %x\n", (unsigned int)(mmap_addr + MMAP_SIZE));
 
 	for (virt_addr = 0; virt_addr < TASK_SIZE; virt_addr += PAGE_SIZE) {
 		index = pgd_index(virt_addr);
-		//printf("index : %lu\n", index);
 		pte_base = (unsigned long *) fake_pgd_base[index];
-		//printf(" pte_base: %lu\n", (unsigned long)pte_base);
 		if (pte_base != NULL) {
 			my_pte_index = pte_index(virt_addr);
-			//printf(" 3: %lu\n", my_pte_index);
 			pte_entry = pte_base[my_pte_index];
-			//printf(" 4: %lu\n", pte_entry);
-			//if (!pte_none(pte_entry)) {
-				if (pte_present(pte_entry)) {
-					page_phy_addr =
-						(pte_entry >> PAGE_SHIFT)
-						<< PAGE_SHIFT;
-					printf("0x%lx 0x%lx 0x%lx %d %d %d %d %d\n"
-							, index,
-							virt_addr,
-							(unsigned long) page_phy_addr,
-							pte_young(pte_entry) ? 1 : 0,
-							pte_file(pte_entry) ? 1 : 0,
-							pte_dirty(pte_entry) ? 1 : 0,
-							pte_write(pte_entry) ? 0 : 1,
-							pte_exec(pte_entry) ? 1 : 0);
+			if (pte_present(pte_entry)) {
+				page_phy_addr =
+					(pte_entry >> PAGE_SHIFT)
+					<< PAGE_SHIFT;
+				printf("0x%lx 0x%lx 0x%lx %d %d %d %d %d\n"
+						, index,
+						virt_addr,
+						(unsigned long) page_phy_addr,
+						pte_young(pte_entry) ? 1 : 0,
+						pte_file(pte_entry) ? 1 : 0,
+						pte_dirty(pte_entry) ? 1 : 0,
+						pte_write(pte_entry) ? 0 : 1,
+						pte_exec(pte_entry) ? 1 : 0);
 
-				} else if (verbose == 1) {
-					page_phy_addr =
-						(pte_entry >> PAGE_SHIFT)
-						<< PAGE_SHIFT;
-					printf("0x%lx 0x%lx %d %d %d %d %d %d\n"
-							, index,
-							virt_addr,
-							0, 0, 0, 0, 0, 0);
-				}
-			//}
+			} else if (verbose == 1) {
+				page_phy_addr =
+					(pte_entry >> PAGE_SHIFT)
+					<< PAGE_SHIFT;
+				printf("0x%lx 0x%lx %d %d %d %d %d %d\n"
+						, index,
+						virt_addr,
+						0, 0, 0, 0, 0, 0);
+			}
 		}
 	}
 
